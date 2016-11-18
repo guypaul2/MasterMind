ed4f342487aa47a480106dfb9b7832d2de175e87
guypaul2 <s.wellenreiter@orange.fr>
2016-11-18 11:24:01 +0100

    Update bd.php

--- modele/bd.php
+++ modele/bd.php
@@ -65,9 +65,51 @@ class BD{
 		"nbCoups" => $jeu->getNbCoups()));
 
 	}
+	//Fonction qui retourne les 5 meilleurs scores et les joueurs associés
+	//pré conditions : il faut qu'il y ai au moins une partie dans la table partie (si il y en a moins que 5 la fonction affiche que les disponnibles
+	//post conditions : retourne les 5 meilleurs scores et les joueurs les ayant joués
+	public function getMeilleursScores() {
+	try{
 
-
-
+	$statement=$this->connexion->query("SELECT pseudo, nombreCoups from parties ORDER BY nombreCoups DESC LIMIT 5 ");
+	
+	while($ligne=$statement->fetch()){
+	$result[]=$ligne['pseudo','nombreCoups'];
+	}
+	return($result);
+	}
+	catch(PDOException $e){
+	    throw new TableAccesException("problème avec la table partie");
+  }
 }
+	//Fonction qui retourne le nombre partie gagnées par un joueur
+	//si le joueur n'a jamais joué retourne une exception
+	//pré-condition : le pseudo du joueur doit exister
+	//post-condition : retourne son nombre de parties gagnées
+	public function getNombrePartieGagnées($pseudo){
+		try{	//on cherche le total de parties gagnées par un joueur (représenté par son pseudo) et on le stock dans nbr_parties_g
+			$statement = $this->connexion->prepare("SELECT SUM(partieGagnee) AS nbr_parties_g FROM parties WHERE pseudo=?;");
+			$statement->bindParam(1, $pseudoParam);
+			$pseudoParam=$pseudo;
+			$statement->execute();
+			$result=$statement->fetch(PDO::FETCH_ASSOC);
+
+			if ($result["nbr_parties_g"]==NUll){//si le joueur n'a pas encore fait de partie on balance une exception
+			throw new PasDePartieException("Ce joueur n'a pas encore joué de parties")
+			}
+			else{//sinon on retourne nbr_parties_g (nbr de partie gagnées
+			return $result["nbr_parties_g"];
+			}
+		}
+		catch(PDOException $e){
+		    $this->deconnexion();
+		    throw new TableAccesException("problème avec la table partie");
+		}
+		
+	}
+	
+	
+
+
 
 ?>
